d269d8ae9f72d93ea72ab5ad45bfbe7f30abe907
sgynb111 <sgynb111@gmail.com>
2024-09-14 10:29:47 +0800

    upload

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,9 @@
 # game/urls.py
 from django.urls import path
-from game.views import index  # 确保 views 文件中定义了 index 视图
+from game.views import index , play  # 确保 views 文件中定义了 index 视图
 
 urlpatterns = [
             path('', index, name='index'),
+            path('play/' , play , name = 'play'),
             ]
 
--- game/views.py
+++ game/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("第一个网页")
+    line1= '<a href = "/play/">play界面</a>'
+    return HttpResponse("game网页" + line1)
 
-
+def play(request):
+    return HttpResponse("play界面")
