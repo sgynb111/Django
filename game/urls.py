c9677545d95c6245de08fa347d0a18beefc548cf
sgynb111 <sgynb111@gmail.com>
2024-09-18 12:41:01 +0800

    create project structure

--- game/urls.py
+++
@@ -1,9 +0,0 @@
-# game/urls.py
-from django.urls import path
-from game.views import index , play  # 确保 views 文件中定义了 index 视图
-
-urlpatterns = [
-            path('', index, name='index'),
-            path('play/' , play , name = 'play'),
-            ]
-
